e5df071e8a0de671e6e90d73e1db063ac57a715f
SyedAyaz3636 <ayaz.syed1612@gmail.com>
2026-08-06 01:11:46 +0530

    Add files via upload
    day 1

---
+++ Week1Day2/datatypes.js
@@ -0,0 +1,20 @@
+var firstName = "Syed Ayaz";
+console.log(firstName);
+console.log(typeof firstName);
+
+let comapany = " JP Morgan "
+console.log(comapany);
+console.log(typeof comapny);
+
+let mobileNum = 1234567891;
+console.log(mobileNum);
+console.log(typeof mobileNum);
+
+let isWorking = true;
+console.log(isWorking);
+console.log(typeof isWorking);
+
+let gender;
+console.log(gender);
+console.log(typeof gender);
+
