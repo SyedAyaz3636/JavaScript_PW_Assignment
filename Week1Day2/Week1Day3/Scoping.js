272699ca7b9c1b11bce2f9764d51594eac863d11
SyedAyaz3636 <ayaz.syed1612@gmail.com>
2026-08-10 02:36:22 +0530

    Add files via upload

---
+++ Week1Day2/Week1Day3/Scoping.js
@@ -0,0 +1,28 @@
+const browserVersion = 'chrome';
+
+function getBrowserVersion () {
+
+    if ( browserVersion === 'chrome')
+        {
+        let browserVersion = 'safari';
+        // console.log(browserVersion)
+
+    }
+    console.log(browserVersion);
+}
+getBrowserVersion();
+
+// const browserVersion = 'chrome';
+
+// function getBrowserVersion() {
+
+//     if ( browserVersion === 'chrome') 
+//         {
+//         let browserVersion = 'safari';
+//         console.log(browserVersion);
+//     }
+
+//     console.log(browserVersion);
+// }
+
+// getBrowserVersion();
---
+++ Week1Day2/Week1Day3/hoisting.js
@@ -0,0 +1,13 @@
+function numberType(number){
+    if(number > 0){
+        console.log("Number is greater than 0");        
+    }
+    else if(number == 0){
+        console.log("Number is equal to 0");        
+    }
+    else{
+        console.log("Number is lesser than 0");
+        
+    }
+}
+numberType(-2)
