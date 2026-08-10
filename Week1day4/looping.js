e4c0e8d9941f93f1e42ddd728d6bf11fd8b23b2c
SyedAyaz3636 <ayaz.syed1612@gmail.com>
2026-08-11 00:12:24 +0530

    Add files via upload

---
+++ Week1day4/looping.js
@@ -0,0 +1,27 @@
+ for (let index = 1; index <= 5; index++) {
+            console.log('I will practice ', index, 'time');
+            }
+
+            //while loop when dont know the count
+
+            let i = 1
+
+            while(i<=5){
+                console.log('The value of i is ', i);
+                i++
+                
+            }
+
+            let pageLoaded = false
+
+            while(!pageLoaded){
+                console.log('waiting for page ..');
+                pageLoaded = true
+                
+            }
+            
+            for ( let j = 1; j <= 5; j++ )
+            {
+                console.log("execute the test cases", j);
+                
+            }
